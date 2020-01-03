732ddff36d83afcc135ff0f6d80acb110ae8359f
cupprint-com <44205059+cupprint-com@users.noreply.github.com>
2020-01-03 14:00:46 +0000

    updated by oliver I think

--- quarantine.php
+++ quarantine.php
@@ -1,11 +1,21 @@
 <?php
+
+$debugOutput=0;
+$typeformUrl = 'https://cupprint.typeform.com/to/ftSFZb';
+	
+$filename=dirname(__FILE__) . '/development.txt';
+if (file_exists($filename)){
+	$debugOutput=1;
+	$typeformUrl = 'https://cupprint.typeform.com/to/yoKheS';
+}
+
 ?>
 
 <html>
 <head>
 <meta name="viewport"
 	content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<title>Cup Quarantine</title>
+<title>Cup Quarantine<?php echo ( $debugOutput==1 ? '-= Development =-' : '' )?></title>
 <style type="text/css">
 html {
 	margin: 0;
@@ -25,7 +35,7 @@ iframe {
 </head>
 <body>
 	<iframe id="typeform-full" width="100%" height="100%" frameborder="0"
-		src="https://cupprint.typeform.com/to/ftSFZb"></iframe>
+		src="<?php echo $typeformUrl; ?>"></iframe>
 	<script type="text/javascript"
 		src="https://embed.typeform.com/embed.js"></script>
 </body>
